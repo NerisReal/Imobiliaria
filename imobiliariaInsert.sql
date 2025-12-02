9413b7b10e1e7b86a0bcee722a3c996d65e2fcb8
Debora Neris <debora.neris@hotmail.com>
2025-12-01 22:30:16 -0300

    Update imobiliariaInsert.sql

--- imobiliariaInsert.sql
+++ imobiliariaInsert.sql
@@ -5,7 +5,6 @@ INSERT INTO proprietario (nome, telefone) VALUES
 ('Ana Paula Rodrigues', '(11) 97788-6655'),
 ('Roberto Almeida', '(11) 95544-3322');
 
----Insert Imovel----
 INSERT INTO imovel (endereco, tipo_imovel, valor, descricao, id_proprietario) VALUES
 ('Rua das Flores, 123 - Jardim América, São Paulo - SP', 'Apartamento', 'R$ 850.000,00', 'Apartamento 3 quartos com 2 vagas de garagem, acabamento premium', 1),
 ('Avenida Paulista, 1000 - Bela Vista, São Paulo - SP', 'Cobertura', 'R$ 2.500.000,00', 'Cobertura duplex com vista panorâmica, 400m²', 2),
@@ -15,8 +14,6 @@ INSERT INTO imovel (endereco, tipo_imovel, valor, descricao, id_proprietario) VA
 ('Rua Haddock Lobo, 300 - Cerqueira César, São Paulo - SP', 'Apartamento', 'R$ 950.000,00', 'Apartamento 2 quartos reformado, pronto para morar', 1),
 ('Avenida Brigadeiro Faria Lima, 1500 - Itaim Bibi, São Paulo - SP', 'Sala Comercial', 'R$ 750.000,00', 'Sala comercial em prédio corporativo classe A', 2);
 
-
------Inserir cliente---
 INSERT INTO cliente (nome, telefone, email, tipo_negocio) VALUES
 ('Fernanda Lima', '(11) 94567-8901', 'fernanda.lima@email.com', 'Compra'),
 ('Ricardo Nunes', '(11) 92345-6789', 'ricardo.nunes@email.com', 'Locação'),
@@ -26,7 +23,7 @@ INSERT INTO cliente (nome, telefone, email, tipo_negocio) VALUES
 ('Roberto Silva', '(11) 98901-2345', 'roberto.silva@email.com', 'Compra'),
 ('Camila Almeida', '(11) 96789-0123', 'camila.almeida@email.com', 'Locação');
 
--- Inserir corretores----
+
 INSERT INTO corretor (nome, creci, telefone) VALUES
 ('Ana Carolina Mendes', 12345, '(11) 91122-3344'),
 ('Pedro Henrique Alves', 23456, '(11) 92233-4455'),
@@ -34,20 +31,10 @@ INSERT INTO corretor (nome, creci, telefone) VALUES
 ('Rafael Gonçalves', 45678, '(11) 94455-6677'),
 ('Mariana Santos', 56789, '(11) 95566-7788');
 
-----Inserir negocio-----
+
 INSERT INTO negocio (tipo_negocio, data, id_imovel, id_cliente, id_corretor,id_proprietario)
 VALUES
 ('Venda', '2024-01-15', 1, 1, 1,1);
-
-Select * From proprietario;
-
-Select * From imovel;
-
-Select * From cliente;
-
-Select * From corretor;
-
-Select * From negocio;
 
 INSERT INTO negocio (tipo_negocio, data, id_imovel, id_cliente, id_corretor,id_proprietario)
 VALUES
